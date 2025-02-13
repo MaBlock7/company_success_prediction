d4d9afa1abdf2bac84b0547ab30b856aa979a4c7
mabolz <112625981+mabolz@users.noreply.github.com>
2025-02-13 17:29:32 +0100

    Added outline of company history creation

---
+++ src/zefix_processing/gender_mapping.py
@@ -0,0 +1,19 @@
+german2gender = {
+    # Nationality
+    'staatsangehörige': 'f',
+    'staatsangehöriger': 'm',
+
+    # Job Titles
+    '...': '...',
+
+}
+
+
+french2gender = {
+    
+}
+
+
+italian2gender = {
+    
+}
